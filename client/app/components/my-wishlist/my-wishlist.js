8ba959c91f5d62ed6fcb07ba3eb1379d51f25935
Ray Kim <rayk1224@yahoo.com>
2016-04-18 08:15:05 -0400

    deleting items added

--- client/app/components/my-wishlist/my-wishlist.js
+++ client/app/components/my-wishlist/my-wishlist.js
@@ -15,7 +15,14 @@ function MyWishListCtrl($scope, $state){
     console.log('edit item')
   }
 
-  $scope.delete_item = function(){
-    console.log('delete item')
+  $scope.delete = function($index){
+    console.log('delete', $index);
+    var item_to_delete = $scope.items[$index];
+    console.log('item to delete', item_to_delete);
+    $scope.items.splice($index, 1);
+
+    // API.delete_item({ name: item_to_delete.name }, function (success) {
+    //   $scope.items.splice($index, 1);
+    // });
   }
 }
--- client/app/components/starred-lists/starred-lists.js
+++ client/app/components/starred-lists/starred-lists.js
@@ -7,7 +7,14 @@ angular
 function StarredLists($scope, $state){
   console.log('starred lists');
 
-  $scope.unstar = function(){
+  $scope.unstar = function($index){
     console.log('unstar wishlist');
+    var wishlist_to_unstar = $scope.items[$index];
+    console.log('wishlist to unstar', wishlist_to_unstar);
+    $scope.items.splice($index, 1)
+
+    // API.unstar_wishlist({ name: wishlist_to_unstar.name }, function (success) {
+    //   $scope.items.splice($index, 1);
+    // });
   }
 }
