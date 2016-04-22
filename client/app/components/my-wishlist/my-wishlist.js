a7f259bc6372cb390dea8c816c55ccf3ffc7943f
Rachel Slater <rachelrobynslater@gmail.com>
2016-04-22 20:47:16 +0100

    lil update before syncing with master

--- client/app/components/my-wishlist/my-wishlist.js
+++ client/app/components/my-wishlist/my-wishlist.js
@@ -45,21 +45,21 @@ function MyWishListCtrl($scope, $state, $auth, $http, $window, UserSvc, $rootSco
     });
   }
 
-  $scope.delete = function($index){
-    // UserSvc.delete_item($index)
-    // .then(function() {
-      console.log('delete', $index);
-      var item_to_delete = $scope.items[$index];
-      console.log('item to delete', item_to_delete);
-      $scope.items.splice($index, 1);
-    // })
-    // .catch(function(err) {
-    //   console.error(err, 'Inside the Wishlist Ctrl, we have an error!');
-    // });
-    // API.delete_item({ name: item_to_delete.name }, function (success) {
-    //   $scope.items.splice($index, 1);
-    // });
-  }
+  // $scope.delete = function($index){
+  //   UserSvc.delete_item($index)
+  //   .then(function() {
+  //     console.log('delete', $index);
+  //     var item_to_delete = $scope.items[$index];
+  //     console.log('item to delete', item_to_delete);
+  //     $scope.items.splice($index, 1);
+  //   })
+  //   .catch(function(err) {
+  //     console.error(err, 'Inside the Wishlist Ctrl, we have an error!');
+  //   });
+  //   // API.delete_item({ name: item_to_delete.name }, function (success) {
+  //   //   $scope.items.splice($index, 1);
+  //   // });
+  // }
 
   $scope.edit = function(item){
     console.log('item to edit', item)
