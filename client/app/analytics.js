a0a76a9b57e05039e25301f66e00c3b76cdba0d2
Ray Kim <rayk1224@yahoo.com>
2016-04-22 19:52:07 -0400

    google analytics comments fixed

--- client/app/analytics.js
+++ client/app/analytics.js
@@ -1,4 +1,4 @@
-// <script>
+<script>
 // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -6,14 +6,14 @@
 
 // ga('create', 'UA-XXXXX-Y', 'auto');
 // ga('send', 'pageview');
-// </script>
+</script>
 
 
-// <!-- Google Analytics (modern) -->
+<!-- Google Analytics (modern) -->
 <script>
 window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
 ga('create', 'UA-XXXXX-Y', 'auto');
 ga('send', 'pageview');
 </script>
 <script async src='https://www.google-analytics.com/analytics.js'></script>
-// <!-- End Google Analytics -->
+<!-- End Google Analytics -->
