2781bbe43b8ec6a3a457bb9183bf7d5b71ee021d
Ray Kim <rayk1224@yahoo.com>
2016-04-22 18:16:11 -0400

    url via email

--- client/app/components/home/home.js
+++ client/app/components/home/home.js
@@ -10,7 +10,6 @@ function HomeCtrl($scope, $state, $auth, $http, UserSvc){
   $scope.authenticate = function(provider, user) {
     //$auth returns a promise. We'll wanna use that, so we have a '.then'. (This is what produces the 'token' object we see in console).
     //Satellizer stores this token for us automatically. (It's in local storage!) It is sent via the request.get in 'auth.js' route.
-    var facebook = 5;
     $auth.authenticate(provider, user)
       .then(function(res) {
         // console.log(res, 'This is the auth response in Home Ctlr.');
@@ -18,12 +17,13 @@ function HomeCtrl($scope, $state, $auth, $http, UserSvc){
         // console.log(token, "This is our token. We're inside Home Ctlr.")
       UserSvc.getProfile()
         .then(function(response) {
-          console.log("This is the UNIQUE ID", response.data._id);
-          var facebook = response.data._id;
-          $state.go('my-wishlist', {id: facebook})
+          var facebookId = response.data._id;
+          // var facebook_name = response.data.displayName;
+          var facebook_email = response.data.email;
+          $state.go('my-wishlist', {id: facebook_email})
         })
         .catch(function(err) {
-          console.error(err, 'Inside the Wishlist Ctrl, we have an error!');
+          console.error(err, 'Inside UserSvc After Auth.authenticate, we have an error!');
         });
     })
     .catch(function(err){
