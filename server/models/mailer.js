018af4d0cc9b48403cbfe7bb2d712450caa7e5cd
Rachel Slater <rachelrobynslater@gmail.com>
2016-07-09 19:02:48 +0100

    updated

--- server/models/mailer.js
+++ server/models/mailer.js
@@ -11,7 +11,7 @@ var mailgun = require('mailgun-js')({
   domain: domain
 });
 
-var sender = 'support@giftgenie.com';
+var sender = 'team@giftgenie.com';
 
 var mailer = {
   sendWelcome: function(user, cb) {
@@ -19,11 +19,10 @@ var mailer = {
       from: sender,
       to: user.email,
       subject: 'Welcome to GiftGenie!',
-      html: "<body><style>body {background-color: #554B4B; text-align:center; border: 5px solid black; font-family:Futura;} h1 {color:white;} h3 {color: #29DCDE} p {color: #29DCDE} h5 {color: lightblue}</style><img src='http://i.imgur.com/vyieP85.png'><h1>Welcome from the Gift Genie team!</h1> <br> <h3>Create your gift wishlist & view your Facebook friends wishlists.</h3> <br> <h1> No more unwanted gifts.</h1> <br> <p>If you have feedback on new features we should add to the app, or comments on it's design & performance, we'd love to hear from you.</p><h5>team@giftgenie.com</h5></body>"
+      html: "<body><style>img {width: 60%; height:60%;}</style><img src='http://i.imgur.com/hoFCY3Y.png'></body>"
     };
     mailgun.messages().send(data, cb);
   }
 }
-
 
 module.exports = mailer;
