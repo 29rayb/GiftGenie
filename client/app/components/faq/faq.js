c90391118a3a5b805cd63de1ce91d4515c236e19
Ray Kim <rayk1224@yahoo.com>
2016-07-11 04:51:57 +0100

    updated

--- client/app/components/faq/faq.js
+++ client/app/components/faq/faq.js
@@ -22,7 +22,9 @@ function faqCtrl($rootScope, $scope){
     {question: "4. Why can't I see anyone in the searchbar?",
       answer: "Only your Facebook friends who are already using the app can be seen" },
     {question: "5. I try to use the app on my iPhone using Safari, but I'm having issues",
-      answer: "Go to settings --> safari --> privacy & security --> block cookies --> allow from websites I visit. Also make sure you have JavaScript enabled in the Advanced Section"}
+      answer: "Go to settings --> safari --> privacy & security --> block cookies --> allow from websites I visit. Also make sure you have JavaScript enabled in the Advanced Section"},
+    {question: "6. It's not working on my web browser (Google Chrome, Safari, FireFox, etc.). How can I fix it?",
+      answer: "Extensions like Adblock might interfere with the Login with Facebook button, preventing you from logginong onto the app"}
   ];
 
   $scope.getAnswer = () => {
--- tmp/js/bundle.js
+++ tmp/js/bundle.js
@@ -567,7 +567,8 @@ function faqCtrl($rootScope, $scope) {
     answer: "Simply click the email icon on the bottom and email us!" }, { question: "3. Can I share this with my friends?",
     answer: "The app is currently in beta; please limit invites to just 5 friends" }, { question: "4. Why can't I see anyone in the searchbar?",
     answer: "Only your Facebook friends who are already using the app can be seen" }, { question: "5. I try to use the app on my iPhone using Safari, but I'm having issues",
-    answer: "Go to settings --> safari --> privacy & security --> block cookies --> allow from websites I visit. Also make sure you have JavaScript enabled in the Advanced Section" }];
+    answer: "Go to settings --> safari --> privacy & security --> block cookies --> allow from websites I visit. Also make sure you have JavaScript enabled in the Advanced Section" }, { question: "6. It's not working on my web browser (Google Chrome, Safari, FireFox, etc.). How can I fix it?",
+    answer: "Extensions like Adblock might interfere with the Login with Facebook button, preventing you from logginong onto the app" }];
 
   $scope.getAnswer = function () {
     $scope.showAnswer ? $scope.showAnswer = false : $scope.showAnswer = true;
