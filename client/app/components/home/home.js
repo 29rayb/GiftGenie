a60c93475c41182af6aa2c9821abbd506163c5e5
Rachel Slater <rachelrobynslater@gmail.com>
2017-01-21 21:50:50 +0000

    update

--- client/app/components/home/home.js
+++ client/app/components/home/home.js
@@ -9,6 +9,7 @@ HomeCtrl.$inject = ['$scope', '$rootScope', '$state', '$auth', '$http', 'UserSvc
 function HomeCtrl($scope, $rootScope, $state, $auth, $http, UserSvc){
 
   $rootScope.loggedIn = localStorage.getItem("satellizer_token");
+  $rootScope.giftGenieLogin = localStorage.getItem('giftGenieLogin');
 
   $scope.authenticate = function(provider, user) {
     $auth.authenticate(provider, user)
@@ -19,10 +20,6 @@ function HomeCtrl($scope, $rootScope, $state, $auth, $http, UserSvc){
 
       var storingIdentifier = res.data.user;
       localStorage.setItem('giftGenieLogin', JSON.stringify(storingIdentifier));
-
-      $rootScope.giftGenieLogin = localStorage.getItem('giftGenieLogin');
-      console.log($rootScope.giftGenieLogin, "HEYYYYYYY");
-
       // is it a problem that when facebook login button clicked, he/she
       // doesn't have the id in the url?
       $state.go('my-wishlist', {id: $rootScope.pro_pic})
